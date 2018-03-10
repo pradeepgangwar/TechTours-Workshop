600b73dbbd2237a4df445436200ed5217ed4b718
pradeepgangwar <pradeepgangwar39@gmail.com>
2018-03-11 02:57:29 +0530

    Added all files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TechTours-Workshop
+
 This repo contains PHP sample code for TechTour Workshop in Aparoksha'18
---
+++ welcome.php
@@ -0,0 +1,87 @@
+<?php
+	
+		session_start();
+		if(!isset($_SESSION['user'])){  
+	  		 echo '<script language="javascript">';
+      	 echo 'alert("What? Dude login first :P")';
+      	 echo '</script>';   
+      	 header("Refresh: 1; url=index.php"); 
+      	 exit();
+		}
+		else{
+			$email = $_SESSION['user'];
+      $id = $_SESSION['id'];
+		}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title> Home Page</title>
+	<!--Stylesheets-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css"/>
+
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Tech Tours</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href=""><?php echo $email ?></a></li>
+        <li><a href="logout.php">Logout</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div class="text-center">
+  <a href="#" class="btn btn-success" role="button">Do something</a>
+</div>
+
+  <h2 class="text-center"> <b>Welcome</b></h2>
+  <br>
+
+
+  
+<footer class="footer">
+      <div class="container">
+        <b><p class="text-muted" style="float: left;">&copy; Pradeep 2017</p></b>
+        <a href="https://github.com/pradeepgangwar/TechTours-Workshop" class="btn btn-success btn-xs text-muted" role="button" style="float: right;"> Source Code </a> 
+        <b><p class="text-muted" style="float: right; padding-right: 10px;">GitHub:</p></b>        
+      </div>
+    </footer>
+ 
+
+<!-- Scripts -->
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+
+</html>
